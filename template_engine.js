586d75767e40d86f2cab3079703ecd402b7cfd15
Christina <christina83@users.noreply.github.com>
2022-01-06 12:22:58 +0100

    add comments

--- template_engine.js
+++ template_engine.js
@@ -6,3 +6,16 @@ const render = (template, data) => {
 }
 
 module.exports.render = render;
+
+
+/* TODO
+- Engine liest template ein
+- Engine spuckt template als array aus
+- Läuft über jedes Element (char) in Array
+- sobald ein % kommt
+    - guckt, ob das Zeichen davor ein < war
+    - wenn ja, guckt, ob das Zeichen danach ein Leerzeichen oder ein = ist
+    - wenn ein = ist, liest die folgende zusammenhängende char Abfolge ein
+    - vergleicht das Wort mit den keys im data object
+    - wenn Wort = key ist, dann wird Wort durch Value ersetzt
+*/
