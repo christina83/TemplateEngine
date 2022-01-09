eba54a36a69da11bace3667c94fceabdd74e5ccc
Christina <christina83@users.noreply.github.com>
2022-01-09 18:11:32 +0100

    trial and error to get the tags

--- template_engine.js
+++ template_engine.js
@@ -1,13 +1,30 @@
 const filesystem = require('fs');
+var DomParser = require('dom-parser');
+var domparser = new DomParser();
 
 function render(template, data) {
-    const content = filesystem.readFileSync(template);
+    const doc = domparser.parseFromString(template, "text/html");
+    console.log(doc);
+    //console.log(doc.lastChild.data);
+
+
+    /* const content = filesystem.readFileSync(template);
     const contentstring = content.toString();
     const chars = contentstring.split('');
-    console.log(chars);
 
-    /* for (let i = 0; i < content.length; i++) {
-        console.log(content[i]);
+    for (let i = 0; i < chars.length; i++) {
+        if(chars[i] == '%') {
+            if(chars[i-1] == '<') {
+                if(chars[i+1] == '=') {
+                    if(chars[i+2] == ' ') {                        
+                        let searchedWord = chars.split('')[3];
+                        console.log(searchedWord);
+                        // Ich könnte das html auch nach tags splicen, dann im entsprechenden tag das ganze einlesen und am ende das _%> 
+                        // abschneiden (map?)
+                    }                    
+                }
+            }
+        }
     } */
 };
 
@@ -17,10 +34,10 @@ exports.render = render;
 /* TODO
 - DONE: Engine liest template ein
 - DONE: Engine spuckt template als array aus
-- Läuft über jedes Element (char) in Array
-- sobald ein % kommt
-    - guckt, ob das Zeichen davor ein < war
-    - wenn ja, guckt, ob das Zeichen danach ein Leerzeichen oder ein = ist
+- DONE: Läuft über jedes Element (char) in Array
+- DONE: sobald ein % kommt
+    - DONE: guckt, ob das Zeichen davor ein < war
+    - DONE: wenn ja, guckt, ob das Zeichen danach ein Leerzeichen oder ein = ist
     - wenn ein = ist, liest die folgende zusammenhängende char Abfolge ein
     - vergleicht das Wort mit den keys im data object
     - wenn Wort = key ist, dann wird Wort durch Value ersetzt
