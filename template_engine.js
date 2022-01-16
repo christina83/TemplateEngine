bc22905e579c23b9797afa5792a37397f0187b7c
Christina <christina83@users.noreply.github.com>
2022-01-16 20:50:06 +0100

    erste regex lösungen

--- template_engine.js
+++ template_engine.js
@@ -1,23 +1,27 @@
 const filesystem = require('fs');
 
-function render(template, data) {
+
+function render(template, data) {   
 
     const content = filesystem.readFileSync(template);
     const contentAsString = content.toString();
-    const charsArray = contentAsString.split('');
 
-    for (let i = 0; i < charsArray.length; i++) {
+    var regex_scriplet = /<%=.*%>/;
+    var regex_expression = /<%.*%>/;
+    var regex_literal = /<.*>/;
 
-        if(javascript_let) {
-            // Keene Ahnung
-        } else if (javascript_expression){
-            // <%= expression %>
-        } else if (javascript_literal) {
-            // <% auszuführender code %>
-        }
-        
+    let scriplet = contentAsString.match(regex_scriplet);
+    if (scriplet) {
+        scriplet = result[0];
+        const index_of_scriplet = result[1];
     }
+
+    // scriplet direkt ersetzen mit regex methode (siehe mdn)
+
+    
+     
 };
+
 
 exports.render = render;
 
@@ -27,12 +31,8 @@ exports.render = render;
 - DONE: Engine spuckt template als array aus
 - DONE: Läuft über jedes Element (char) in Array
 - Entscheidet, ob einer der 3 Fälle eingetreten ist durch das char
-- DONE: sobald ein % kommt
-    - DONE: guckt, ob das Zeichen davor ein < war
-    - DONE: wenn ja, guckt, ob das Zeichen danach ein Leerzeichen oder ein = ist
-    - wenn ein = ist, liest die folgende zusammenhängende char Abfolge ein
-    - vergleicht das Wort mit den keys im data object
-    - wenn Wort = key ist, dann wird Wort durch Value ersetzt
+- danach das Literal, die Expression oder das Scriplet mit Hilfe der 3 Funktionen einlesen
+
 - Immer auskommentierten Code am Ende des Arbeitstages löschen (intellektuelle und emotionale Belästigung)
 - if Schleifen geraten außerhalb des Codes (mit i-1 bei index 0)
 - ich habe die verzweigung am anfang nicht richtig gemacht mit den 3 fällen, über die ich an relevantem js-code stolpern kann (plain html, 
