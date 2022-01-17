8282fd326a627e31720919c06e0fdafbfcc35822
Christina <christina83@users.noreply.github.com>
2022-01-17 17:28:03 +0100

    regex weiter entwickelt (vor Call mit Phillip)

--- template_engine.js
+++ template_engine.js
@@ -6,19 +6,23 @@ function render(template, data) {
     const content = filesystem.readFileSync(template);
     const contentAsString = content.toString();
 
-    var regex_scriplet = /<%=.*%>/;
-    var regex_expression = /<%.*%>/;
-    var regex_literal = /<.*>/;
+    var regex_scriplet = /<%=.*%>/; // Was charakterisiert diesen Fall, was bei den anderen nicht auftritt?
+    var regex_expression = /<%^=.*%>/;
+    var regex_literal = />^%^=.*</; // diesen Fall brauche ich doch eigentlich gar nicht
 
     let scriplet = contentAsString.match(regex_scriplet);
-    if (scriplet) {
-        scriplet = result[0];
-        const index_of_scriplet = result[1];
-    }
-
-    // scriplet direkt ersetzen mit regex methode (siehe mdn)
-
-    
+    let expression = contentAsString.match(regex_expression);
+    let literal = contentAsString.match(regex_literal);
+    if (literal) {
+        console.log(literal[0]);  
+        if(scriplet) {
+            console.log(scriplet[0]); 
+        } else if (expression) {
+            console.log(expression[0]); 
+        }    
+    } else {
+        console.log('Noch drüber nachdenken')
+    }    
      
 };
 
@@ -32,10 +36,8 @@ exports.render = render;
 - DONE: Läuft über jedes Element (char) in Array
 - Entscheidet, ob einer der 3 Fälle eingetreten ist durch das char
 - danach das Literal, die Expression oder das Scriplet mit Hilfe der 3 Funktionen einlesen
+- scriplet direkt ersetzen mit regex methode (siehe mdn)?
+- if(drittes), dadrin if(erstes), elseif (das zweite), else
 
-- Immer auskommentierten Code am Ende des Arbeitstages löschen (intellektuelle und emotionale Belästigung)
-- if Schleifen geraten außerhalb des Codes (mit i-1 bei index 0)
-- ich habe die verzweigung am anfang nicht richtig gemacht mit den 3 fällen, über die ich an relevantem js-code stolpern kann (plain html, 
-    js-ausdruck und js-auszuführender code) 
-- stattdessen bin ich direkt in den einen spezialfall reingegangen (js-ausdruck)
+
 */
